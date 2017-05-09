21d23581cfee9bbc3d10e383d57380e8fb84a16f
BLACKROCK92 <perezdaffunchio@gmail.com>
2017-05-08 22:44:00 -0300

    Menu Commit

---
+++ VR Architect Prototype/Assets/Scripts/GameController_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController_Script : MonoBehaviour {
+
+    public Menu_Script menuScript;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.M) && !menuScript.activeMenu.Equals("none"))
+        {
+            menuScript.DeactivateMenu("none");
+        }
+        else if(Input.GetKeyUp(KeyCode.M) && menuScript.activeMenu.Equals("none"))
+        {
+            menuScript.ActivateMenu("Menu");
+        }
+
+
+
+    }
+
+}
---
+++ VR Architect Prototype/Assets/Scripts/Menu_Script.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Script : MonoBehaviour
+{
+
+    public GameObject MenuObject;
+    public GameObject FurnitureSubMenuObject;
+    public GameObject StructureSubMenuObject;
+    public GameObject MiscellaneousSubMenuObject;
+    public string activeMenu;
+
+    private void Start()
+    {
+        activeMenu = "none";
+    }
+
+    public void ActivateMenu(string active)
+    {
+        activeMenu = active;
+        MenuObject.SetActive(true);
+        FurnitureSubMenuObject.SetActive(false);
+        StructureSubMenuObject.SetActive(false);
+        MiscellaneousSubMenuObject.SetActive(false);
+        PrintButtonMessage("Activate Menu");
+    }
+    public void DeactivateMenu(string active)
+    {
+        activeMenu = active;
+        MenuObject.SetActive(false);
+        FurnitureSubMenuObject.SetActive(false);
+        StructureSubMenuObject.SetActive(false);
+        MiscellaneousSubMenuObject.SetActive(false);
+        PrintButtonMessage("Deactivate Menu");
+    }
+
+    public void ActivateFurnitureSubMenu(string active)
+    {
+        activeMenu = active;
+        MenuObject.SetActive(false);
+        FurnitureSubMenuObject.SetActive(true);
+        StructureSubMenuObject.SetActive(false);
+        MiscellaneousSubMenuObject.SetActive(false);
+        PrintButtonMessage(active);
+    }
+
+    public void ActivateStructureSubMenu(string active)
+    {
+        activeMenu = active;
+        MenuObject.SetActive(false);
+        FurnitureSubMenuObject.SetActive(false);
+        StructureSubMenuObject.SetActive(true);
+        MiscellaneousSubMenuObject.SetActive(false);
+        PrintButtonMessage(active);
+    }
+
+    public void ActivateMiscellaneousSubMenu(string active)
+    {
+        activeMenu = active;
+        MenuObject.SetActive(false);
+        FurnitureSubMenuObject.SetActive(false);
+        StructureSubMenuObject.SetActive(false);
+        MiscellaneousSubMenuObject.SetActive(true);
+        PrintButtonMessage(active);
+    }
+
+    public void PrintButtonMessage(string Message)
+    {
+        print(Message);
+    }
+}
