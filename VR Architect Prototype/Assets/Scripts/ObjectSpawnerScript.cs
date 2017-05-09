769f63db33fde7ac1aa8eb0dc05e505c2da3a53f
Nahuel Castillo <nahushadow@gmail.com>
2017-05-08 22:39:56 -0300

    Agregado el script de Spawn de Objetos, Modelos 3d y Prefabs.

---
+++ VR Architect Prototype/Assets/Scripts/ObjectSpawnerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawnerScript : MonoBehaviour {
+
+
+    public void InstantiateObject(GameObject obj) {
+
+        Instantiate(obj);
+
+    } 
+}
