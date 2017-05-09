eefe7a8fe0815a9a8922edbd6282a0b5aaca3990
BLACKROCK92 <perezdaffunchio@gmail.com>
2017-05-08 22:59:29 -0300

    Instantiate objects with buttons

--- VR Architect Prototype/Assets/Scripts/ObjectSpawner_Script.cs
+++ VR Architect Prototype/Assets/Scripts/ObjectSpawner_Script.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSpawnerScript : MonoBehaviour {
+public class ObjectSpawner_Script : MonoBehaviour {
 
 
     public void InstantiateObject(GameObject obj) {
