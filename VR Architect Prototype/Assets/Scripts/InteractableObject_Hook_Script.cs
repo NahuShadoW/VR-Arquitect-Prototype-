9bf8188af8e4089892916d4d2699ed1d9c568c2a
Motts <mnd.matias.diaz@gmail.com>
2017-05-26 19:51:15 -0300

    Hook, toggle gravedad, kinematics y colliders

---
+++ VR Architect Prototype/Assets/Scripts/InteractableObject_Hook_Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using VRTK.GrabAttachMechanics;
+
+public class InteractableObject_Hook_Script : MonoBehaviour
+{
+
+    [SerializeField]
+    private VRTK_InteractableObject InteractableObjScript;
+    public float speed = 5;
+    public Transform target;
+
+    private void Start()
+    {
+        InteractableObjScript = GetComponent<VRTK_InteractableObject>();
+        target = Camera.main.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if (InteractableObjScript.IsGrabbed())
+        {
+            var controllerEvents = InteractableObjScript.GetGrabbingObject().GetComponent<VRTK_ControllerEvents>();
+            if (controllerEvents.buttonTwoPressed)
+            {
+                Destroy(this.gameObject.GetComponent<Joint>(), 0.1f);
+                //this.transform.position = InteractableObjScript.GetGrabbingObject().transform.position;
+                this.transform.position = Vector3.Lerp(this.transform.position, GameObject.Find("Spawner_chico").transform.position, 0.05f);
+                if (GameObject.Find("playerScale").GetComponent<PhysicsMenu_Script>().isKinematicOn)
+                    this.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+        else
+        {
+            this.gameObject.AddComponent<Joint>();
+        }
+    }
+
+}
---
+++ VR Architect Prototype/Assets/Scripts/PhysicsMenu_Script.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhysicsMenu_Script : MonoBehaviour
+{
+
+    public bool isGravityOn = true;
+    public bool isKinematicOn = true;
+    public bool isColliderOn = true;
+
+    public void ToggleGravity()
+    {
+        if (!isGravityOn)
+        {
+            if (isColliderOn)
+            {
+                Physics.gravity = new Vector3(0, -9.81f, 0);
+                isGravityOn = true;
+            }
+            else
+            {
+                print("Los colliders estan desactivados, la gravedad no se puede activar");
+                GameObject.Find("GravityToggle").GetComponent<Toggle>().isOn = false;
+            }
+        }
+        else
+        {
+            Physics.gravity = new Vector3(0, 0, 0);
+            isGravityOn = false;
+        }
+    }
+
+    public void ToggleKinematics()
+    {
+        if (!isKinematicOn)
+        {
+            var objs = GameObject.FindGameObjectsWithTag("InteractableObject");
+            foreach (var rb in objs)
+            {
+                rb.GetComponent<Rigidbody>().isKinematic = true;
+            }
+            isKinematicOn = true;
+            objs = null;
+        }
+        else
+        {
+            var objs = GameObject.FindGameObjectsWithTag("InteractableObject");
+            foreach (var rb in objs)
+            {
+                rb.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            isKinematicOn = false;
+            objs = null;
+        }
+    }
+
+    public void ToggleColliders()
+    {
+
+        if (!isColliderOn)
+        {
+            var objs = GameObject.FindGameObjectsWithTag("InteractableObject");
+            foreach (var obj in objs)
+            {
+                foreach (BoxCollider bc in obj.GetComponents<BoxCollider>())
+                {
+                    bc.enabled = true;
+                }
+            }
+            isColliderOn = true;
+            objs = null;
+        }
+        else
+        {
+            if (!isGravityOn)
+            {
+                var objs = GameObject.FindGameObjectsWithTag("InteractableObject");
+                foreach (var obj in objs)
+                {
+                    foreach (BoxCollider bc in obj.GetComponents<BoxCollider>())
+                    {
+                        bc.enabled = false;
+                    }
+                }
+                isColliderOn = false;
+                objs = null;
+            }
+            else
+            {
+                print("Gravedad activada, los colliders no se pueden desactivar");
+                GameObject.Find("collidersToggle").GetComponent<Toggle>().isOn = true;
+            }
+        }
+    }
+}
